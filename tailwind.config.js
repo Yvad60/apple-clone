15c87fd3bf7e291aa2b5789193b51c92ec49ad17
ivad <yvesyvad@gmail.com>
2022-09-14 15:57:41 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary_black: '#313132',
+      },
+    },
+  },
+  plugins: [],
+};
